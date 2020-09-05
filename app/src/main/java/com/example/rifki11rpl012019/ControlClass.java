d12567d4943bcc947bca058327beb85987f1aa23
rifkihariyanto25 <hariyantorifki25@gmail.com>
2020-09-05 15:02:38 +0700

    add SP

---
+++ app/src/main/java/com/example/rifki11rpl012019/ControlClass.java
@@ -0,0 +1,29 @@
+package com.example.rifki11rpl012019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class ControlClass extends AppCompatActivity {
+    private SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sharedPreferences = getSharedPreferences( "login", Context.MODE_PRIVATE);
+        if (sharedPreferences.getString("username" , "").isEmpty() ||
+        sharedPreferences.getString("username" , "").equalsIgnoreCase(""))  {
+
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }else{
+            startActivity(new Intent(getApplicationContext(),MainMenu.class));
+            finish();
+
+
+
+        }
+    }
+}
--- app/src/main/java/com/example/rifki11rpl012019/MainActivity.java
+++ app/src/main/java/com/example/rifki11rpl012019/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.rifki11rpl012019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+    EditText txtusername;
+    EditText txtpassword;
+    Button btnlogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref = getSharedPreferences("login", MODE_PRIVATE);
+        txtusername = (EditText)findViewById(R.id.txtusername);
+        txtpassword = (EditText)findViewById(R.id.txtpassword);
+        btnlogin = (Button)findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txtusername.getText().toString().equalsIgnoreCase("admin")
+                        && txtpassword.getText().toString().equalsIgnoreCase("admin")){
+                    //saving ke sp
+                    editor = pref.edit();;
+                    editor.putString("username" , txtusername.getText().toString());
+                    editor.putString("status" , "login");
+                    editor.apply();
+                    //menuju main menu
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                }
+            }
+        });
     }
 }
